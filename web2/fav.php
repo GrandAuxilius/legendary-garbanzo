ab02a92f0aaa3782c823c7a3fb5bd687b9248903
GrandAuxilius <202080129@psu.palawan.edu.ph>
2023-03-02 15:07:03 +0800

    Done with my Part

--- web2/fav.php
+++ web2/fav.php
@@ -1,8 +1,17 @@
 <html>
 <head><title>Your Favorite</title></head>
 <body>
+
     <?php
         
+ if (isset($_POST['username']) && !empty($_POST['username']) && isset($_POST['color']) && !empty($_POST['color']) && isset($_POST['dish']) && !empty($_POST['dish'])) {
+    echo "Thanks for your selection " . $_POST['username'] . '<br> You really enjoyed ' . $_POST['dish'] . ' <br> - especially with a nice ' . $_POST['color'] . ' wine.';
+} else {
+    echo 'Please enter your name and select your favorite wine and dish';
+}
+
+
     ?>
+
 </body>
 </html>
--- web3/calc.php
+++ web3/calc.php
@@ -1,19 +1,34 @@
 <html>
 <head><title>Calculation Result</title></head>
 <body>
-    <?php
-        # test if input is_numeric
-        if(1){
-            # test if calc is not null
-            if(1){
-                # switch statement for value of calc
-                switch(1){
-                    
-                }
-                echo "Calculation result: $result";
+    
+<?php
+    # test if input is_numeric
+    if(is_numeric($_POST['val1']) && is_numeric($_POST['val2'])){
+        # test if calc is not null
+        if(!empty($_POST['calc'])){
+            # switch statement for value of calc
+            switch($_POST['calc']){
+                case 'add':
+                    $result = $_POST['val1'] + $_POST['val2'];
+                    break;
+                case 'sub':
+                    $result = $_POST['val1'] - $_POST['val2'];
+                    break;
+                case 'mul':
+                    $result = $_POST['val1'] * $_POST['val2'];
+                    break;
+                case 'div':
+                    $result = $_POST['val1'] / $_POST['val2'];
+                    break;
             }
+            echo "Calculation result: $result";
         }
-        else{ echo "Invalid entry - please retry"; }
-    ?>
+    }
+    else{ echo "Invalid entry - please retry"; 
+    }
+
+?>
+    
 </body>
 </html>
